2cc0bfb3bfcc50dac454365f05d9d29cb8147ef5
Phoulos <145342442+Phoulos@users.noreply.github.com>
2023-12-18 10:25:37 +0100

    finished step1

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,15 +1,18 @@
-CREATE TABLE boat (
-  id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
-);
+CREATE TABLE
+    boat (
+        id INT AUTO_INCREMENT NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        coord_x INT NOT NULL,
+        coord_y INT NOT NULL,
+        PRIMARY KEY(id)
+    );
 
-CREATE TABLE tile (
-  id INT AUTO_INCREMENT NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
-);
+CREATE TABLE
+    tile (
+        id INT AUTO_INCREMENT NOT NULL,
+        type VARCHAR(255) NOT NULL,
+        coord_x INT NOT NULL,
+        coord_y INT NOT NULL,
+        has_treasure boolean DEFAULT false,
+        PRIMARY KEY(id)
+    );
