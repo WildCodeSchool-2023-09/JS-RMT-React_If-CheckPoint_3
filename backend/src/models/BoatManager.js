4885622fd15a88d0d6f56e4522dc14379dcff46b
Leslie2186 <leslie.moraud@gmail.com>
2023-12-18 11:24:50 +0100

    Finish step 5

--- backend/src/models/BoatManager.js
+++ backend/src/models/BoatManager.js
@@ -7,7 +7,9 @@ class BoatManager extends AbstractManager {
 
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    const [rows] = await this.database.query(
+      `select boat.id, tile.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure from ${this.table} INNER JOIN tile`
+    );
 
     // Return the array of boats
     return rows;
