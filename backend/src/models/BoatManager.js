adc4d6a1127aa24a3d2818fb638277b729a4e994
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-18 15:48:21 +0100

    finish step bonus

--- backend/src/models/BoatManager.js
+++ backend/src/models/BoatManager.js
@@ -6,11 +6,17 @@ class BoatManager extends AbstractManager {
     super({ table: "boat" });
   }
 
-  async readAll() {
+  async readAll(where) {
+    let query = `select boat.id, tile.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure from ${this.table} inner join tile on boat.coord_x = tile.coord_x and boat.coord_y = tile.coord_y`;
+    const value = [];
+
+    if (where && where.name) {
+      query += " where name = ?";
+      value.push(where.name);
+    }
+
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(
-      `select boat.id, tile.id, boat.coord_x, boat.coord_y, boat.name, tile.type, tile.has_treasure from ${this.table} inner join tile on boat.coord_x = tile.coord_x and boat.coord_y = tile.coord_y`
-    );
+    const [rows] = await this.database.query(query, value);
 
     // Return the array of boats
     return rows;
