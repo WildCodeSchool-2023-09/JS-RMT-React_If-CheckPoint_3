a726745d4c2e546f721d8b83ad12e55d6c4bf1a1
iNaeRoZ <naeroz@outlook.fr>
2023-12-18 11:09:56 +0100

    step 3 ok + img MCD

--- backend/src/models/BoatManager.js
+++ backend/src/models/BoatManager.js
@@ -12,6 +12,19 @@ class BoatManager extends AbstractManager {
     // Return the array of boats
     return rows;
   }
+
+  async update(id, coordX, coordY) {
+    try {
+      const [affectedRows] = await this.database.query(
+        "UPDATE boat SET coord_x=? , coord_y=? WHERE id=?",
+        [coordX, coordY, id]
+      );
+      return affectedRows;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 }
 
 module.exports = BoatManager;
